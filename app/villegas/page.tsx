f217020ef8a874d5d6794c23b8c411ca49a590db
Roselle Anne Villegas <roselleanne07villegas@gmail.com>
2025-09-16 19:42:37 +0800

    added villegas page

---
+++ app/villegas/page.tsx
@@ -0,0 +1,10 @@
+import Info from "./info";
+export default function Page() {
+
+    return (
+        <div>
+        <h1 className="text=3x1 font-bold">Roselle Anne M. Villegas</h1>     
+        <Info />
+        </div>
+    );
+}
